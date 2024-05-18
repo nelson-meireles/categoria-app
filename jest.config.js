1d48a83a91b2149256b019b0ff50647158c16399
Nelson Meireles <nelsonmeireles@me.com>
2024-05-18 13:12:26 -0300

    Atualização testes

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/tests'],
-    moduleFileExtensions: ['ts', 'js'],
-    transform: {
-      '^.+\\.ts$': 'ts-jest'
-    },
-    testMatch: ['**/?(*.)+(spec|test).ts']
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  testMatch: ['**/?(*.)+(spec|test).ts'],
+  collectCoverage: true, 
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};
